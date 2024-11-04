ff091d80bbf7b882d3763c472f4f4a2576a2d7ed
lukajk1 <73144336+lukajk1@users.noreply.github.com>
2024-11-04 02:29:09 -0800

    add basic frontend crud functionality

--- public/js/list-item.js
+++ public/js/list-item.js
@@ -1,53 +1,113 @@
-class listItem {
+class ListItem {
     isEditMode = false;
-    constructor(siteTitle, siteURL, startpage) {
+    constructor(siteTitle, siteURL) {
         this.siteTitle = siteTitle;
         this.siteURL = siteURL;
         this.startpage = startpage;
 
         this.li = document.createElement('li');
-        this.mainList = startpage.mainList;
+        this.mainList = document.getElementById('main-list');
+        this.mainList.appendChild(this.li);
 
-        mainList.appendChild(this.li);
+        // Construct HTML elements
+        this.initLink();
+        this.initInputFields();
+        this.initEditOptions();
+
+        this.mainLink = this.li.querySelector('.link-label');
+        this.inputFields = this.li.querySelector('.input-fields');
+        this.editOptions = this.li.querySelector('.edit-options');
+
+        this.inputSiteName = this.li.querySelector('input[name="siteName"]');
+        this.inputSiteName.value = siteTitle;
+        this.inputSiteURL = this.li.querySelector('input[name="siteURL"]');
+        this.inputSiteURL.value = siteURL;
+
     }
 
-    setEditMode(boolValue) {
-        isEditMode = boolValue;
+    initLink() {
+        const linkHTML = `
+            <span class="link-label">
+                <a href="${this.siteURL || '#'}">${this.siteTitle || 'unnamed link'}</a>
+            </span>
+        `;
+        this.li.insertAdjacentHTML('beforeend', linkHTML);
     }
 
-    defaultLinkDisplayHTML = `
-        <span class="link-label"><a href="#">${this.siteTitle}</a></span>
-    `;
+    initInputFields() {
+        const inputFieldsHTML = `
+            <span class="input-fields" style="display:none;">
+                <input type="text" name="siteName" size="24" placeholder="site name" autocomplete="off" />
+                <br>
+                <input type="text" name="siteURL" size="24" placeholder="site url" autocomplete="off" />
+            </span>
+        `;
+        this.li.insertAdjacentHTML('beforeend', inputFieldsHTML);
+    }
 
-    li = '';
-    link = '';
-    editOptions = '';
-    inputFields = '';
+    initEditOptions() {
+        const editOptionsHTML = `
+            <span class='edit-options' style="display:none; position: relative; top: -40px;">
+                <a href='#' class='options-style'id='delete'>[ - ]</a>
+            </span>
+        `;
+
+        // older version
+        //const editOptionsHTML = `
+        //    <span class='edit-options' style="display:none;">
+        //        <a href='#' class='options-style'>archive</a>
+        //        <a href='#' class='options-style' id='save'>edit</a>
+        //        <a href='#' class='options-style' id='delete'>delete</a>
+        //    </span>
+        //`;
+        this.li.insertAdjacentHTML('beforeend', editOptionsHTML);
+
+        //this.li.querySelector('#save').addEventListener('click', () => this.toggleEditMode());
+        this.li.querySelector('#delete').addEventListener('click', () => this.delete());
+    }
+
+    setFieldsDisplay(show) {
+        if (show) {
+            this.inputFields.style.display = 'block';
+        } else {
+            this.inputFields.style.display = 'none';
+            this.saveFields();
+        }
+    }
+
+    setEditMode(value) {
+        this.isEditMode = value;
+        this.setFieldsDisplay(this.isEditMode);
+        this.editOptions.style.display = this.isEditMode ? 'block' : 'none';
+        this.mainLink.style.display = this.isEditMode ? 'none' : 'block';
+    }
+
+    saveFields() {
+        const siteNameInput = this.inputFields.querySelector('input[name="siteName"]');
+        const siteURLInput = this.inputFields.querySelector('input[name="siteURL"]');
+
+        // Update the values
+        if (siteNameInput && siteURLInput) {
+            this.siteTitle = siteNameInput.value || 'unnamed link';
+            this.siteURL = siteURLInput.value;
+
+            // Update the displayed link
+            const linkElement = this.li.querySelector('.link-label a');
+            if (linkElement) {
+                linkElement.textContent = this.siteTitle;
+                linkElement.href = this.siteURL;
+            }
+        }
+    }
 
     delete() {
         this.li.remove();
-        // removes the parent <li> which deletes all the child elements
-        // later on this needs to meet an express endpoint to remove the list item from the json
     }
 }
+
 
 class startpage {
     constructor(mainList) {
         this.mainList = mainList;
     }
-    editOptionsHTML = `
-        <span class='edit-options'>
-            <a href='#' class='options-style'>archive</a>
-            <a href='#' class='options-style'>edit</a>
-            <a href='#' class='options-style'>delete</a>
-        </span>
-    `;
-
-    inputFieldsHTML = `
-        <input type="text" name="siteName" size="10" placeholder="site name" autocomplete="off" />
-        <br>
-        <input type="text" name="siteURL" size="10" placeholder="site url" autocomplete="off" />
-    `;
-
-
 }
